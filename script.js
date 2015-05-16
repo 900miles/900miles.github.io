dc4fb570966553ac38d265656cbf0bd8d7ab163a
900miles <milesbsilva@yahoo.com>
2015-05-15 20:47:14 -0700

    More Styling and added Transitions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    $('div.head').hide().fadeIn('slow');
+    $('div.container').hide().fadeIn(250);
+    
+    $('a').click(function(event) {
+        event.preventDefault();
+        newLocation = this.href;
+        $('div.container').fadeOut(250, newpage);
+    });
+
+    function newpage() {
+        window.location = newLocation;
+    }
+
 });
