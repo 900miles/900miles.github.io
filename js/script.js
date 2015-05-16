5eaf090f5592e7c1c5e334eb944b49785c66a954
900miles <milesbsilva@yahoo.com>
2015-05-16 16:52:50 -0700

    A bit of fade effect changes.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('div.container').hide().fadeIn(250);
     
-    $('a').click(function(event) {
+    $('a.fade').click(function(event) {
         event.preventDefault();
         newLocation = this.href;
         $('div.container').fadeOut(250, newpage);
